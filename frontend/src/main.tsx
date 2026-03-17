0d0553cd538dd6f354f657a32ba4b597de6c4a14
amannkumar <amannkumar3895@gmail.com>
2026-03-16 21:28:30 -0400

    feat: add Microsoft Clarity analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import Clarity from "@microsoft/clarity";
-
-Clarity.init("vtbi00udwj");
 
 createRoot(document.getElementById("root")!).render(<App />);
